3fbfcdfcd93431519af3d911479db330bb4657e0
Reza Husain <84816920+rezahusain@users.noreply.github.com>
2024-02-19 21:37:49 +0000

    Finished product

--- app.rb
+++ app.rb
@@ -2,8 +2,47 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:rules)
+end
+
+get("/rock") do
+  @player1 = "Rock"
+  choices = ["Rock", "Paper", "Scissors"]
+  @player2 = choices.sample
+  if @player1 == "Rock" && @player2 == "Rock"
+    @outcome = "We tied!"
+  elsif @player1 == "Rock" && @player2 == "Paper"
+    @outcome = "We lost!"
+  else
+    @outcome = "We won!"
+  end
+  erb(:rock)
+end
+
+get("/paper") do
+  @player1 = "Paper"
+  choices = ["Rock", "Paper", "Scissors"]
+  @player2 = choices.sample
+  if @player1 == "Paper" && @player2 == "Rock"
+    @outcome = "We won!"
+  elsif @player1 == "Paper" && @player2 == "Paper"
+    @outcome = "We tied!"
+  else
+    @outcome = "We lost!"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do
+  @player1 = "Scissors"
+  choices = ["Rock", "Paper", "Scissors"]
+  @player2 = choices.sample
+  if @player1 == "Scissors" && @player2 == "Rock"
+    @outcome = "We lost!"
+  elsif @player1 == "Scissors" && @player2 == "Paper"
+    @outcome = "We won!"
+  else
+    @outcome = "We tied!"
+  end
+  erb(:scissors)
 end
